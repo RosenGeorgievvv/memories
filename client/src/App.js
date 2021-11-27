6af89a2d7d31d437ea79e1be35c2f8f0da6a7661
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-11-27 17:37:33 +0200

    Client component structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
 import React from "react";
+import { Container, AppBar, Typography, Grid } from '@material-ui/core';
+import memories from './images/memories.png';
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 
 
 const App = () => {
     return ( <
-        div >
+        Container maxidth = "lg" >
         <
-        h1 > App < /h1> <
-        /div>
+        AppBar position = "static"
+        color = "inherit" >
+        <
+        Typography variant = "h2"
+        align = "center" > Memories < /Typography> <
+        img src = { memories }
+        alt = "memories"
+        height = "60" / >
+        <
+        /AppBar> <
+        Container >
+        <
+        Grid container justify = "space-between"
+        alignItems = "stretch"
+        spacing = { 3 } >
+        <
+        Grid item xs = { 12 }
+        sm = { 7 } >
+        <
+        Posts / >
+        <
+        /Grid> <
+        Grid item xs = { 12 }
+        sm = { 4 } >
+        <
+        Form / >
+        <
+        /Grid> <
+        /Grid> <
+        /Container> <
+        /Container>
     )
 }
 
